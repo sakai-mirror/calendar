ee2bd2c69b32603b534cffd4411c42222b94d60c
nuno@ufp.pt <nuno@ufp.pt@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-02-25 14:07:03 +0000

    SAK-12852: iCal External Subscription support in Schedule Tool
    git-svn-id: https://source.sakaiproject.org/svn/calendar/branches/SAK-12852@41565 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- calendar-impl/impl/src/java/org/sakaiproject/calendar/impl/BaseExternalCalendarSubscriptionService.java
+++ calendar-impl/impl/src/java/org/sakaiproject/calendar/impl/BaseExternalCalendarSubscriptionService.java
@@ -393,13 +393,13 @@ public class BaseExternalCalendarSubscriptionService implements ExternalCalendar
 		// use Base64
 		byte[] encoded = CommonsCodecBase64.encodeBase64(url.getBytes());
 		// '/' cannot be used in Reference => use '.' instead (not part of Base64 alphabet)
-		String encStr = new String(encoded).replaceAll("/", ".");
+		String encStr = new String(encoded).replaceAll("/", "\\.");
 		return encStr;
 	}
 	
 	public String getSubscriptionUrlFromId(String id) {
 		// use Base64
-		byte[] decoded = CommonsCodecBase64.decodeBase64(id.replaceAll(".", "/").getBytes());
+		byte[] decoded = CommonsCodecBase64.decodeBase64(id.replaceAll("\\.", "/").getBytes());
 		return new String(decoded);
 	}
 	
