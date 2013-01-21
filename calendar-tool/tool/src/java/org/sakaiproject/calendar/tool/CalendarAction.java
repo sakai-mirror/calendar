8d7e83a408f8dbd8d3cdd593cd738003e77d40bf
steve.swinsburg@gmail.com <steve.swinsburg@gmail.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2013-01-21 04:59:39 +0000

    SAK-22979 merge trunk r118119
    git-svn-id: https://source.sakaiproject.org/svn/calendar/branches/sakai-2.8.x@118525 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- calendar-tool/tool/src/java/org/sakaiproject/calendar/tool/CalendarAction.java
+++ calendar-tool/tool/src/java/org/sakaiproject/calendar/tool/CalendarAction.java
@@ -2932,10 +2932,15 @@ extends VelocityPortletStateAction
 				
 				String ownerId = calEvent.getCreator();
 				if ( ownerId != null && ! ownerId.equals("") )
+				// if the user not defined, assigned the owner_name as ""
+				try 
 				{
 					String ownerName = 
 							 UserDirectoryService.getUser( ownerId ).getDisplayName();
 					context.put("owner_name", ownerName);
+				} 
+				catch (UserNotDefinedException e) {
+					context.put("owner_name", "");
 				}
 				
 				String siteName = calEvent.getSiteName();
@@ -2964,12 +2969,6 @@ extends VelocityPortletStateAction
 				context.put(NO_EVENT_FLAG_CONTEXT_VAR, TRUE_STRING);
 			}
 			catch (PermissionException e)
-			{
-				context.put(ALERT_MSG_KEY,rb.getString("java.alert.younotpermadd"));
-				M_log.debug(".buildDescriptionContext(): " + e);
-				return;
-			}
-			catch (UserNotDefinedException e)
 			{
 				context.put(ALERT_MSG_KEY,rb.getString("java.alert.younotpermadd"));
 				M_log.debug(".buildDescriptionContext(): " + e);
